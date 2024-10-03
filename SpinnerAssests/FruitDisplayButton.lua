6e9248965e299d2f9b65f545b1236d9d858745c4
Nikolas Posavec <nikolas.posavec@gmail.com>
2024-10-03 12:23:56 -0400

    made fruitdisplaybutton consistent with spinbutton updates 10/3

--- SpinnerAssests/FruitDisplayButton.lua
+++ SpinnerAssests/FruitDisplayButton.lua
@@ -2,9 +2,9 @@ local button = script.Parent
 local rarityEvent = game:GetService("ReplicatedStorage"):WaitForChild("RarityPicked")  -- Reference the BindableEvent
 
 -- Function to update the FRUIT button text when the event is triggered
-local function onRarityReceived(rarity)
-	button.Text = rarity  -- Set the button text to the received rarity
-	print("Rarity received: " .. rarity)
+local function onRarityReceived(rarity, fruit)
+	button.Text = fruit .. " (" .. rarity .. ")"  -- Set button text to display fruit and rarity
+	print("Rarity: " .. rarity .. ", Fruit: " .. fruit)
 end
 
 -- Connect the BindableEvent to the function
